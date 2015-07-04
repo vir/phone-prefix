fee8d0291f642ed1a71e20c9b2515a5b5e7f5893
Vasily i. Redkin <vir@ctm.ru>
2015-07-05 00:21:18 +0300

    Added resulting loc table dump, just in case

--- dump_database.sh
+++ dump_database.sh
@@ -2,18 +2,18 @@
 #
 # (c) vir
 #
-# Last modified: 2015-07-05 00:19:41 +0300
+# Last modified: 2015-07-05 00:29:40 +0300
 #
 
 DIR=$(dirname $(readlink -f "$0"))
 cd $DIR
-
+mkdir -p results
 psql $* << ***
 \o results/loc.sql
 \qecho BEGIN;
 \qecho DELETE FROM loc;
 \qecho 'COPY loc (pref, len, beg, fin, op, loc) FROM stdin;'
-COPY (SELECT * FROM loc ORDER BY pref, fin) TO STDOUT; -- WITH CSV HEADER;
+COPY (SELECT * FROM loc ORDER BY pref, fin, op, loc) TO STDOUT; -- WITH CSV HEADER;
 \qecho '\\\\.'
 \qecho COMMIT;
 ***
