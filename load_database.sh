9f5e487111fcf6bbccb5e19784237ffe1513fef4
Vasily i. Redkin <vir@ctm.ru>
2015-07-07 13:24:14 +0300

    Separated "reg" field (Region) from "loc" field)

--- load_database.sh
+++ load_database.sh
@@ -2,7 +2,7 @@
 #
 # (c) vir
 #
-# Last modified: 2015-07-03 19:40:43 +0300
+# Last modified: 2015-07-06 11:24:34 +0300
 #
 
 DIR=$(dirname $(readlink -f "$0"))
@@ -25,19 +25,24 @@ CREATE TEMP TABLE wpr(pr TEXT, wplink TEXT, loc TEXT);
 CREATE TEMP TABLE c(ac TEXT, beg TEXT, fin TEXT, num INTEGER, op TEXT, loc TEXT);
 \copy c FROM '$C/Kody_ABC-3kh.csv' (FORMAT CSV, DELIMITER ';', ENCODING 'Windows-1251', HEADER, QUOTE '@');
 \copy c FROM '$C/Kody_ABC-4kh.csv' (FORMAT CSV, DELIMITER ';', ENCODING 'Windows-1251', HEADER, QUOTE '@');
-\copy c FROM '$C/Kody_ABC-8kh.csv' (FORMAT CSV, DELIMITER ';', ENCODING 'Windows-1251', HEADER);
+\copy c FROM '$C/Kody_ABC-8kh.csv' (FORMAT CSV, DELIMITER ';', ENCODING 'Windows-1251', HEADER, QUOTE '@');
 \copy c FROM '$C/Kody_DEF-9kh.csv' (FORMAT CSV, DELIMITER ';', ENCODING 'Windows-1251', HEADER, QUOTE '@');
+
+-- latest updates
+UPDATE c SET loc = REGEXP_REPLACE(loc, ' - ', '-', 'g');
+ALTER TABLE c ADD reg TEXT;
+UPDATE c SET reg = REGEXP_REPLACE(loc, '(.*?)\s*\|\s*(.*)', '\2'), loc = REGEXP_REPLACE(loc, '^(.*?)\s*\|\s*(.*)$', '\1') WHERE loc LIKE '%|%';
 
 -- \i prc.sql
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS loc(pref TEXT, len SMALLINT, beg TEXT, fin TEXT, op TEXT, loc TEXT);
+CREATE TABLE IF NOT EXISTS loc(pref TEXT, len SMALLINT, beg TEXT, fin TEXT, op TEXT, loc TEXT, reg TEXT);
 DELETE FROM loc;
-INSERT INTO loc(len, beg, fin, op, loc) SELECT 2 + LENGTH(ac) + LENGTH(fin), '+7' || ac || beg, '+7' || ac || fin, op, loc FROM c;
+INSERT INTO loc(len, beg, fin, op, loc, reg) SELECT 2 + LENGTH(ac) + LENGTH(fin), '+7' || ac || beg, '+7' || ac || fin, op, loc, reg FROM c;
 INSERT INTO loc(pref, loc) SELECT pr, loc FROM wpr;
 
 CREATE OR REPLACE FUNCTION loc(num TEXT) RETURNS TEXT AS \$\$
-	SELECT concat_ws(E'\n', op, loc) FROM loc WHERE (LENGTH(\$1) = len AND \$1 BETWEEN beg AND fin) OR (\$1 LIKE pref || '%') ORDER BY LENGTH(fin) DESC, LENGTH(pref);
+	SELECT concat_ws(E'\n', op, loc, reg) FROM loc WHERE (LENGTH(\$1) = len AND \$1 BETWEEN beg AND fin) OR (\$1 LIKE pref || '%') ORDER BY LENGTH(fin) DESC, LENGTH(pref);
 \$\$ LANGUAGE SQL;
 
 COMMIT;
