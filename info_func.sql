c9392a454e588a904ceafbd333ce2dbff8b5b087
Vasily i. Redkin <vir@ctm.ru>
2015-07-09 13:53:09 +0300

    Updated info_func result format for latest Dyatel

--- info_func.sql
+++ info_func.sql
@@ -1,8 +1,15 @@
 
 CREATE OR REPLACE FUNCTION info(num TEXT) RETURNS TEXT AS $$
 	-- Timezone in Postgresql expressed to the WEST from UTC!
-	SELECT concat_ws(E'\n', op, loc, loc.reg, 'Временная зона: UTC'||TO_CHAR(tz.tz, 'SGFM99'),
-		'Местное время: '||TO_CHAR(CURRENT_TIMESTAMP AT TIME ZONE ('UTC'||TO_CHAR(-tz.tz, 'SGFM99')), 'HH24:MI'))
+	SELECT concat_ws(E'\n'
+		, 'operator ' || op
+		, 'loc ' || loc.loc
+		, 'reg ' || loc.reg
+		, 'timezone UTC'||TO_CHAR(tz.tz, 'SGFM99')
+		, 'localtime '||TO_CHAR(CURRENT_TIMESTAMP AT TIME ZONE ('UTC'||TO_CHAR(-tz.tz, 'SGFM99')), 'HH24:MI:SS')
+		, 'latitude '||tz.lat
+		, 'longitude '||tz.lon
+		)
 	FROM loc LEFT JOIN tz
 		ON (tz.np = loc.loc AND (loc.reg IS NULL OR loc.reg LIKE '%' || tz.reg OR tz.reg LIKE loc.reg || '%' OR tz.reg = loc.loc))
 		OR ((loc.loc LIKE tz.np||' %' OR loc.loc LIKE tz.np||'ая область') AND loc.reg IS NULL)
